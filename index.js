938322879bab67b2b8f9042ede94c5c7022dd080
Little Hacker <119156098+LittleHacker999@users.noreply.github.com>
2024-12-17 17:26:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -162,8 +162,8 @@ async function connectToWA() {
     setInterval(checkForUpdates, 20000); // Check for new news every 20 seconds
 }
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.listen(8000, () => {
+    console.log('Server is running on http://localhost:8000');
 });
 
 connectToWA();
